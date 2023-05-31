b746d7d5957611478551c0f1e6c452f3fc865c88
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-05-31 05:05:52 +0300

    lib update

--- Rede.h
+++ Rede.h
@@ -1056,7 +1056,8 @@ void RedeSourceIterator_moveCursorBack(RedeSourceIterator* iterator, size_t shif
 
 #if defined(REDE_DO_LOGS)
     #define LOGS_SCOPE(name)\
-        char* logs__scope__name = #name;\
+        const char* logs__scope__name = #name;\
+        if(!logs__scope__name[0]) logs__scope__name = __func__;\
         printf("LOGS '%s'\n", logs__scope__name);\
 
     #define LOG(...)\
@@ -1164,7 +1165,7 @@ void RedeSourceIterator_moveCursorBack(RedeSourceIterator* iterator, size_t shif
 
 
 int RedeDest_init(RedeDest* dest) {
-    LOGS_SCOPE(RedeDest_init);
+    LOGS_SCOPE();
 
     dest->index = -1;
 
@@ -1189,7 +1190,7 @@ int RedeDest_init(RedeDest* dest) {
 }
 
 void RedeDest_destroy(RedeDest* dest) {
-    LOGS_SCOPE(RedeDest_destroy);
+    LOGS_SCOPE();
     if(dest->type == RedeDestTypeFile) {
         fclose(dest->data.file.fp);
     }
@@ -1764,83 +1765,6 @@ RedeExpressionWriteStatus RedeCompilerHelpers_writeFunctionCall(
 }
 
 
-RedeWriteStatus RedeCompilerHelpers_writeIfStatement(
-    RedeSourceIterator* iterator,
-    RedeCompilationMemory* memory,
-    RedeDest* dest,
-    RedeCompilationContext* ctx
-) {
-    LOGS_SCOPE(writeIfStatement);
-
-    CHECK(RedeDest_writeByte(dest, REDE_CODE_JUMP_IF_NOT), "Failed to write REDE_CODE_JUMP_IF_NOT");
-
-    ctx->isIfStatementArgument = 1;
-        int status = RedeCompilerHelpers_writeExpression(iterator, memory, dest, ctx);
-        CHECK(status, "Failed to write the condition")
-    ctx->isIfStatementArgument = 0;
-
-    if(status == RedeExpressionWriteStatusFunction) {
-        CHECK(RedeDest_writeByte(dest, REDE_CODE_JUMP_IF_NOT), "Failed to write REDE_CODE_JUMP_IF_NOT after the function call");
-        CHECK(RedeDest_writeByte(dest, REDE_TYPE_STACK), "Failed to write REDE_TYPE_STACK after the function call");
-    }
-
-    CHECK(RedeDest_writeByte(dest, REDE_DIRECTION_FORWARD), "Failed to write REDE_DIRECTION_FORWARD");
-    CHECK(RedeDest_writeByte(dest, 0), "Failed to write jump length placeholder first byte");
-    size_t firstJumpSizeByte = dest->index;
-    CHECK(RedeDest_writeByte(dest, 0), "Failed to write jump length placeholder second byte");
-
-
-
-    char ch;
-    while((ch = RedeSourceIterator_nextChar(iterator))) {
-        LOG_LN("Char: '%c'(%d)", ch, ch);
-
-
-        int isOpenBracket = ch == '(';
-        if(
-            (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
-            ||
-            ch == '('
-        ) {
-            RedeWriteStatus resultStatus = RedeWriteStatusOk;
-
-            if(isOpenBracket) {
-                LOG_LN("Multiple statements");
-
-                ctx->bracketsBlockDepth++;
-                CHECK(RedeCompilerHelpers_writeStatements(iterator, memory, dest, ctx), "Failed to write if multiple statements");
-                ctx->bracketsBlockDepth--;
-            } else {
-                LOG_LN("Single statement");
-                
-                RedeSourceIterator_moveCursorBack(iterator, 1);
-                resultStatus = RedeCompilerHelpers_writeStatement(iterator, memory, dest, ctx);
-                CHECK(resultStatus, "Failed to write if single statement");
-            }
-
-            size_t diff = dest->index - (firstJumpSizeByte + 1);
-            if(diff > 0xFFFF) {
-                LOG_LN("The if body is to big to jump forward");
-                return RedeWriteStatusError;
-            }
-
-            LOG_LN("Jump size: %zu", diff);
-
-            unsigned char* diffBytes = (unsigned char*)&diff;
-
-            CHECK(RedeDest_writeByteAt(dest, firstJumpSizeByte + 0, diffBytes[0]), "Failed to write jump size first byte");
-            CHECK(RedeDest_writeByteAt(dest, firstJumpSizeByte + 1, diffBytes[1]), "Failed to write jump size second byte");
-
-            return resultStatus;
-        }
-    }
-
-
-    LOG_LN("Unexpected end of the input");
-    return RedeWriteStatusError;
-}
-
-
 RedeExpressionWriteStatus RedeCompilerHelpers_writeOperationWithToken(
     RedeSourceIterator* iterator, 
     RedeCompilationMemory* memory, 
@@ -2293,6 +2217,83 @@ RedeWriteStatus RedeCompilerHelpers_writeWhile(
 }
 
 
+RedeWriteStatus RedeCompilerHelpers_writeIfStatement(
+    RedeSourceIterator* iterator,
+    RedeCompilationMemory* memory,
+    RedeDest* dest,
+    RedeCompilationContext* ctx
+) {
+    LOGS_SCOPE(writeIfStatement);
+
+    CHECK(RedeDest_writeByte(dest, REDE_CODE_JUMP_IF_NOT), "Failed to write REDE_CODE_JUMP_IF_NOT");
+
+    ctx->isIfStatementArgument = 1;
+        int status = RedeCompilerHelpers_writeExpression(iterator, memory, dest, ctx);
+        CHECK(status, "Failed to write the condition")
+    ctx->isIfStatementArgument = 0;
+
+    if(status == RedeExpressionWriteStatusFunction) {
+        CHECK(RedeDest_writeByte(dest, REDE_CODE_JUMP_IF_NOT), "Failed to write REDE_CODE_JUMP_IF_NOT after the function call");
+        CHECK(RedeDest_writeByte(dest, REDE_TYPE_STACK), "Failed to write REDE_TYPE_STACK after the function call");
+    }
+
+    CHECK(RedeDest_writeByte(dest, REDE_DIRECTION_FORWARD), "Failed to write REDE_DIRECTION_FORWARD");
+    CHECK(RedeDest_writeByte(dest, 0), "Failed to write jump length placeholder first byte");
+    size_t firstJumpSizeByte = dest->index;
+    CHECK(RedeDest_writeByte(dest, 0), "Failed to write jump length placeholder second byte");
+
+
+
+    char ch;
+    while((ch = RedeSourceIterator_nextChar(iterator))) {
+        LOG_LN("Char: '%c'(%d)", ch, ch);
+
+
+        int isOpenBracket = ch == '(';
+        if(
+            (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+            ||
+            ch == '('
+        ) {
+            RedeWriteStatus resultStatus = RedeWriteStatusOk;
+
+            if(isOpenBracket) {
+                LOG_LN("Multiple statements");
+
+                ctx->bracketsBlockDepth++;
+                CHECK(RedeCompilerHelpers_writeStatements(iterator, memory, dest, ctx), "Failed to write if multiple statements");
+                ctx->bracketsBlockDepth--;
+            } else {
+                LOG_LN("Single statement");
+                
+                RedeSourceIterator_moveCursorBack(iterator, 1);
+                resultStatus = RedeCompilerHelpers_writeStatement(iterator, memory, dest, ctx);
+                CHECK(resultStatus, "Failed to write if single statement");
+            }
+
+            size_t diff = dest->index - (firstJumpSizeByte + 1);
+            if(diff > 0xFFFF) {
+                LOG_LN("The if body is to big to jump forward");
+                return RedeWriteStatusError;
+            }
+
+            LOG_LN("Jump size: %zu", diff);
+
+            unsigned char* diffBytes = (unsigned char*)&diff;
+
+            CHECK(RedeDest_writeByteAt(dest, firstJumpSizeByte + 0, diffBytes[0]), "Failed to write jump size first byte");
+            CHECK(RedeDest_writeByteAt(dest, firstJumpSizeByte + 1, diffBytes[1]), "Failed to write jump size second byte");
+
+            return resultStatus;
+        }
+    }
+
+
+    LOG_LN("Unexpected end of the input");
+    return RedeWriteStatusError;
+}
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -2303,7 +2304,7 @@ RedeWriteStatus RedeCompilerHelpers_writeWhile(
     goto exit_compiler;\
 
 int Rede_compile(RedeSource* src, RedeCompilationMemory* memory, RedeDest* dest) {
-    LOGS_SCOPE(Rede_compile);
+    LOGS_SCOPE();
     int compilationStatus = 0;
 
     RedeCompilationContext ctx = {
@@ -2347,6 +2348,10 @@ exit_compiler:
     return compilationStatus;
 }
 #endif // REDE_COMPILER_IMPLEMENTATION
+
+
+
+
 #if !defined(REDE_STD_H)
 #define REDE_STD_H
 
